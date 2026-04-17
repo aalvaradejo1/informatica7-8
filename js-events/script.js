6ba27e98c81aa2d6ac53cc7b7687a08127719356
Alyssa Alvaradejo <assylalva@gmail.com>
2026-04-17 17:29:22 +0000

    events

---
+++ js-events/script.js
@@ -0,0 +1,25 @@
+function greet(name) {
+    //console.log(`Hello ${name}`);
+    console.log("Hello " + name);
+}
+
+greet("Alyssa");
+
+// function addNumbers(num1, num2) {
+//     let sum = num1 + num2;
+//     alert("Sum is equal to " + sum);
+// }
+
+//addNumbers(6,7);
+
+function article1(){
+    alert("1 We believe in God the Eternal Father, and His Son, Jesus Christ, and the Holy Ghost.");
+}
+
+function article2(){
+    alert("2 We believe that men will be punished for their own sins, and not for Adam’s transgression..");
+}
+
+function article3(){
+    alert("3 We believe that through the Atonement of Christ, all mankind may be saved, by obedience to the laws and ordinances of the Gospel.");
+}
