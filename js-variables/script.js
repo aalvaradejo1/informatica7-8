c15602416ed826582f07f37c8f438cf3345eb876
Alyssa Alvaradejo <assylalva@gmail.com>
2026-04-22 20:18:18 +0000

    listo

--- js-variables/script.js
+++ js-variables/script.js
@@ -1,6 +1,26 @@
-const firstName = "Alyssa";
-const favColor = "Green";
-let location = "School";
-let mood = "Tired";
-console.log(firstName);
+function function1(){
+    const firstName = "Alyssa";
+    const favColor = "Green";
+    let location = "School";
+    let mood = "Tired";
+    location = "House";
+    console.log(firstName + " " + favColor + " " + location +  " " + mood);
+}
 
+function function2(){
+    let companyName = "La Froyeria";
+    let foundingYear = 2024;
+    let isActive = true;
+    let fundingAmount;
+    console.log(companyName + " " + foundingYear + " " + isActive +  " " + fundingAmount)
+}
+
+function function3(){
+    let num1 = 5;
+    let num = num1;
+    num = num * 2;
+    num = num + 10;
+    num = num / 2;
+    num = num - num1;
+    alert(num);
+}
