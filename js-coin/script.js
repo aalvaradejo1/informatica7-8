a2c5f1315a7cb63ece01c9768672d99860ecd259
Alyssa Alvaradejo <assylalva@gmail.com>
2026-04-29 20:18:19 +0000

    guess coinflip

--- js-coin/script.js
+++ js-coin/script.js
@@ -1,12 +1,23 @@
-function coin(){
+function coin(user){
     let num = Math.random();
     console.log(num);
 
-    let title = document.querySelector("h1");
+    let title = document.querySelector("h2");
+    let computer;
 
     if (num > 0.5) {
         title.innerHTML = "Heads";
+        computer = "Heads";
     }else {
         title.innerHTML = "Tails";
+        computer = "Tails";
+    }
+
+    let result = document.querySelector("h3");
+
+    if (computer == user) {
+        result.innerHTML = "You Won!";
+    } else{
+        result.innerHTML = "You Lost:(";
     }
 }
