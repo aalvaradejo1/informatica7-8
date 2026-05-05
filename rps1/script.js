472fb7851ba98ff55f1a75b2319d58873735938f
Alyssa Alvaradejo <assylalva@gmail.com>
2026-05-05 16:29:20 +0000

    examen corregido no terminado

--- rps1/script.js
+++ rps1/script.js
@@ -15,16 +15,24 @@ function coin(user){
         title.innerHTML = "Scissors";
         computer = "Scissors";
     }
+}
+
+// document.addEventListener("DOMContentLoaded", function(){
+//     //Your code here
+//     document.querySelector("#rock-button").onclick=coin("Rock");
+
+// });
 
 
     let result = document.querySelector("h3");
 
-    if (user == computer) {
-        result.innerHTML="Tie";
-    } else if (user == "Rock"){
-        if ( computer == "Paper"){
-            result.innerHTML= "You lose";
-        } else {
-            result.innerHTML="You won";
-        }
+    if (user > computer) {
+        result.innerHTML = "You Won!!";
+    } else if (user == computer) {
+        result.innerHTML = "You Tied";
+    }else if (user == "Rock", computer == "Scissors") {
+        result.innerHTML = "You Won!!";
+    } else {
+        result.innerHTML = "You Lost:(";
     }
+
